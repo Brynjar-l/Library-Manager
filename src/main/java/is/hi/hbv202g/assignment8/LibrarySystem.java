6603bd8df179d7237d7c5af1bb410e3cebd62c85
Brynjar-l <brynjar.sighvatsson@gmail.com>
2024-04-20 00:03:26 +0000

    Generated JavaDocs for LibrarySystem.java

--- src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
@@ -1,163 +1,207 @@
 package is.hi.hbv202g.assignment8;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.time.LocalDate;
+
+/**
+ * Represents a Library Management System that allows for managing books, users, and lendings.
+ */
 public class LibrarySystem {
 
     private final List<User> users = new ArrayList<>();
     private final List<Book> books = new ArrayList<>();
     private final List<Lending> lendings = new ArrayList<>();
 
-
-
+    /**
+     * Constructs an empty LibrarySystem.
+     */
     public LibrarySystem() {
     }
 
+    /**
+     * Returns a list of all books in the library.
+     *
+     * @return the list of books
+     */
     public List<Book> getBooks() {
         return books;
     }
+
+    /**
+     * Returns a list of all users registered in the library.
+     *
+     * @return the list of users
+     */
     public List<User> getUsers() {
         return users;
     }
 
+    /**
+     * Adds a new book with a single author to the library.
+     *
+     * @param title The title of the book
+     * @param authorName The name of the author
+     */
     public void addBookWithTitleAndNameOfSingleAuthor(String title, String authorName) {
         books.add(new Book(title, authorName));
     }
+
+    /**
+     * Adds a new book with multiple authors to the library.
+     *
+     * @param title The title of the book
+     * @param authors A list of authors of the book
+     * @throws EmptyAuthorListException If the author list is empty
+     */
     public void addBookWithTitleAndAuthorList(String title, List<Author> authors) throws EmptyAuthorListException {
         if (authors.isEmpty()) throw new EmptyAuthorListException("Empty Authors list");
         books.add(new Book(title, authors));
     }
+
+    /**
+     * Adds a new book with one or more authors specified as varargs.
+     *
+     * @param title The title of the book
+     * @param authors Varargs of authors' names
+     */
     public void addBookWithTitleAndUnspecifiedAmountOfAuthors(String title, String... authors) {
         books.add(new Book(title, authors));
     }
+
+    /**
+     * Removes a book from the library by its name.
+     *
+     * @param bookName The name of the book to be removed
+     */
     public void removeBookFromDatabase(String bookName) {
-        for (Book book : books) {
-            if (book.getTitle().equalsIgnoreCase(bookName)) {
-                books.remove(book);
-            }
-        }
+        books.removeIf(book -> book.getTitle().equalsIgnoreCase(bookName));
     }
 
-
-    public void removeBookFromDatabase(Book bookName) {
-        for (Book book : books) {
-            if (book.equals(bookName)) {
-                books.remove(bookName);
-            }
-        }
+    /**
+     * Removes a specific book object from the library.
+     *
+     * @param book The book to be removed
+     */
+    public void removeBookFromDatabase(Book book) {
+        books.remove(book);
     }
 
+    /**
+     * Registers a new student user to the library.
+     *
+     * @param name The name of the student
+     * @param feePaid Indicates whether the student has paid the library fee
+     */
     public void addStudentUser(String name, boolean feePaid) {
         users.add(new Student(name, feePaid));
     }
+
+    /**
+     * Registers a new faculty member user to the library.
+     *
+     * @param name The name of the faculty member
+     * @param department The department where the faculty member works
+     */
     public void addFacultyMemberUser(String name, String department) {
         users.add(new FacultyMember(name, department));
     }
 
+    /**
+     * Finds a book by its title.
+     *
+     * @param title The title of the book to find
+     * @return The book found
+     * @throws UserOrBookDoesNotExistException If the book does not exist
+     */
     public Book findBookByTitle(String title) throws UserOrBookDoesNotExistException {
-        for (Book book : books) {
-            if (book.getTitle().equalsIgnoreCase(title)) {
-                return book;
-            }
-        }
-        throw new UserOrBookDoesNotExistException("Book does not exist");
+        return books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElseThrow(() -> new UserOrBookDoesNotExistException("Book does not exist"));
     }
+
+    /**
+     * Finds a user by their name.
+     *
+     * @param name The name of the user to find
+     * @return The user found
+     * @throws UserOrBookDoesNotExistException If the user does not exist
+     */
     public User findUserByName(String name) throws UserOrBookDoesNotExistException {
-        for (User user : users) {
-            if (user.getName().equals(name)) {
-                return user;
-            }
-        }
-        throw new UserOrBookDoesNotExistException("User does not exist");
+        return users.stream()
+                .filter(user -> user.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new UserOrBookDoesNotExistException("User does not exist"));
     }
+
+    /**
+     * Allows a student to borrow a book from the library.
+     *
+     * @param user The student borrowing the book
+     * @param book The book to be borrowed
+     */
     public void borrowBook(Student user, Book book) {
         lendings.add(new Lending(book, user));
     }
 
-    /*
-    public void borrowBook(Student user, String bookName) {
-        for (Book book : books) {
-            if (book.getTitle().equalsIgnoreCase(bookName) && !book.isLent() && user.isFeePaid()) {
-                user.setFeePaid(false);
-                lendings.add(new Lending(book, user));
-                book.setLentTo(user);
-            }
-        }
-    }
-
+    /**
+     * Retrieves a user by their name.
+     *
+     * @param name The name of the user to retrieve
+     * @return The user retrieved
+     * @throws UserOrBookDoesNotExistException If the user does not exist
      */
-
     public User getUser(String name) throws UserOrBookDoesNotExistException {
-        for (User user : users) {
-            if (user.getName().equalsIgnoreCase(name)) {
-                return user;
-            }
-        }
-        throw new UserOrBookDoesNotExistException("User does not exist");
+        return findUserByName(name);
     }
 
+    /**
+     * Retrieves a book by its name.
+     *
+     * @param bookName The name of the book to retrieve
+     * @return The book retrieved
+     * @throws UserOrBookDoesNotExistException If the book does not exist
+     */
     public Book getBook(String bookName) throws UserOrBookDoesNotExistException {
-        for (Book book : books) {
-            if (book.getTitle().equalsIgnoreCase(bookName)) {
-                return book;
-            }
-        }
-        throw new UserOrBookDoesNotExistException("Book does not exist");
+        return findBookByTitle(bookName);
     }
 
-    public boolean inLending(Book name) {
-        for (Lending len : lendings) {
-            if (len.getBook().equals(name)) {
-                return true;
-            }
-        }
-        return false;
+    /**
+     * Checks if a book is currently on lend.
+     *
+     * @param book The book to check
+     * @return true if the book is on lend, false otherwise
+     */
+    public boolean inLending(Book book) {
+        return lendings.stream().anyMatch(lending -> lending.getBook().equals(book));
     }
+
+    /**
+     * Searches for a lending record of a specific book.
+     *
+     * @param book The book to search for in lending records
+     * @return The lending record found
+     * @throws UserOrBookDoesNotExistException If the book is not on lendings list
+     */
     public Lending searchLending(Book book) throws UserOrBookDoesNotExistException {
-        for (Lending len: lendings) {
-            if (len.getBook().equals(book)) {
-                return len;
-            }
-        }
-        throw new UserOrBookDoesNotExistException("Book is not on lendings list");
+        return lendings.stream()
+                .filter(lending -> lending.getBook().equals(book))
+                .findFirst()
+                .orElseThrow(() -> new UserOrBookDoesNotExistException("Book is not on lendings list"));
     }
 
-
+    /**
+     * Allows a user to return a borrowed book.
+     *
+     * @param book The book to be returned
+     * @throws UserOrBookDoesNotExistException If the book was never lent
+     */
     public void returnBook(Book book) throws UserOrBookDoesNotExistException {
-
-        if (inLending(book)) {
-            Lending lending = searchLending(book);
-            lending.getUser().returnBook();
-            lending.getBook().returned();
-
-            lendings.remove(lending);
-
-        } else {
-
-
-            throw new UserOrBookDoesNotExistException("Book was never lent");
-        }
-
+        Lending lending = searchLending(book);
+        lending.getUser().returnBook();
+        lending.getBook().returned();
+        lendings.remove(lending);
     }
 
-    public static void main(String[] args) throws UserOrBookDoesNotExistException {
-        LibrarySystem lib = new LibrarySystem();
-        lib.addStudentUser("B", true);
-        lib.addBookWithTitleAndUnspecifiedAmountOfAuthors("1900", "Brynjar");
-        lib.borrowBook(((Student)lib.getUser("B")), lib.getBook("1900"));
-
-        System.out.println("BOOK IS BORROWED? " + lib.inLending(lib.getBook("1900")));
-
-        lib.returnBook(lib.getBook("1900"));
-
-
-    }
-
-    public void returnBook(String book) {
-        for (Lending lending : lendings) {
-            if (lending.getBook().getTitle().equalsIgnoreCase(book)) {
-                lendings.remove(lending);
-            }
-        }
-    }
 }
